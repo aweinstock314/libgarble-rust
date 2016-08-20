d8e161a9ff623380903e93a7dc30e5976c57e1fc
Avi Weinstock <aweinstock314@gmail.com>
2016-08-19 21:28:41 -0400

    Added garble_seed and supporting AES infrastructure.

--- src/garble.rs
+++ src/garble.rs
@@ -1,5 +1,7 @@
 use libc::{c_int, c_void, calloc, posix_memalign};
+use openssl_sys::RAND_bytes;
 use simd::u8x16;
+use std::cell::UnsafeCell;
 use std::mem;
 use std::ptr;
 
@@ -38,7 +40,7 @@ pub struct GarbleCircuit {
 #[no_mangle] pub extern fn garble_allocate_blocks(nblocks: usize) -> *mut Block {
     let mut blocks: *mut c_void = ptr::null_mut();
     let res = unsafe { posix_memalign(&mut blocks, mem::align_of::<Block>(), nblocks * mem::size_of::<Block>()) };
-    println!("garble_allocate_blocks: {}, {:p}", res, blocks);
+    println!("garble_allocate_blocks({}): {}, {:p}", nblocks, res, blocks);
     if res == 0 {
         blocks as *mut Block
     } else {
@@ -112,8 +114,79 @@ pub struct GarbleCircuit {
 #[no_mangle] pub extern fn garble_save() {
     panic!("garble_save");
 }
-#[no_mangle] pub extern fn garble_seed() {
-    panic!("garble_seed");
+
+#[derive(Debug)]
+struct AesKeyInner {
+    rd_key: [Block; 11],
+    rounds: usize
+}
+struct AesKey {
+    inner: UnsafeCell<AesKeyInner>
+}
+unsafe impl Sync for AesKey {}
+
+// unsafe global variable for compatibility with the C library
+static RAND_AES_KEY: AesKey = AesKey { inner: UnsafeCell::new(
+    AesKeyInner { rd_key: [Block::splat(0); 11], rounds: 0 }
+)};
+
+fn aes_set_encrypt_key(userkey: Block, key: &mut AesKeyInner) {
+    macro_rules! expand_assist {
+        ($v1:ident, $v2:ident, $v3:ident, $v4:ident, $shuff_const:expr, $aes_const:expr) => {
+            unsafe {
+                asm!(concat!("aeskeygenassist $$", $aes_const, ", %xmm1, %xmm2") : "={xmm2}"($v2) : "{xmm1}"($v4));
+                asm!("shufps $$16, %xmm1, %xmm3" : "={xmm3}"($v3) : "{xmm3}"($v3), "{xmm1}"($v1));
+                asm!("pxor %xmm3, %xmm1" : "={xmm1}"($v1) : "{xmm1}"($v1), "{xmm3}"($v3));
+                asm!("shufps $$140, %xmm1, %xmm3" : "={xmm3}"($v3) : "{xmm3}"($v3), "{xmm1}"($v1));
+                asm!("pxor %xmm3, %xmm1" : "={xmm1}"($v1) : "{xmm1}"($v1), "{xmm3}"($v3));
+                asm!(concat!("pshufd $$", $shuff_const, ", %xmm2, %xmm2") : "={xmm2}"($v2) : "{xmm2}"($v2));
+                asm!("pxor %xmm2, %xmm1" : "={xmm1}"($v1) : "{xmm1}"($v1), "{xmm2}"($v2));
+            }
+        }
+    }
+    let mut x0 = userkey;
+    let mut x1: Block;
+    let mut x2 = Block::splat(0);
+    key.rd_key[0] = x0;
+    expand_assist!(x0, x1, x2, x0, "255", "1");
+    key.rd_key[1] = x0;
+    expand_assist!(x0, x1, x2, x0, "255", "2");
+    key.rd_key[2] = x0;
+    expand_assist!(x0, x1, x2, x0, "255", "4");
+    key.rd_key[3] = x0;
+    expand_assist!(x0, x1, x2, x0, "255", "8");
+    key.rd_key[4] = x0;
+    expand_assist!(x0, x1, x2, x0, "255", "16");
+    key.rd_key[5] = x0;
+    expand_assist!(x0, x1, x2, x0, "255", "32");
+    key.rd_key[6] = x0;
+    expand_assist!(x0, x1, x2, x0, "255", "64");
+    key.rd_key[7] = x0;
+    expand_assist!(x0, x1, x2, x0, "255", "128");
+    key.rd_key[8] = x0;
+    expand_assist!(x0, x1, x2, x0, "255", "27");
+    key.rd_key[9] = x0;
+    expand_assist!(x0, x1, x2, x0, "255", "54");
+    key.rd_key[10] = x0;
+    key.rounds = 10;
+}
+
+#[no_mangle] pub extern fn garble_seed(seed: *mut Block) -> Block {
+    println!("garble_seed({:p})", seed);
+    let mut cur_seed: Block = unsafe { mem::uninitialized() };
+    match unsafe { seed.as_mut() } {
+        Some(seed) => { cur_seed = *seed; }
+        None => {
+            if unsafe { RAND_bytes(mem::transmute(&mut cur_seed), 16) } == 0 {
+                println!("RAND_bytes failed");
+                cur_seed = Block::splat(0);
+            }
+        }
+    }
+    let aes: &mut AesKeyInner = unsafe { RAND_AES_KEY.inner.get().as_mut().unwrap() };
+    aes_set_encrypt_key(cur_seed, aes);
+    println!("seeded: {:?}", aes);
+    cur_seed
 }
 #[no_mangle] pub extern fn garble_size() {
     panic!("garble_size");
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
+#![feature(asm,const_fn)]
 extern crate libc;
+extern crate openssl_sys;
 extern crate simd;
 
 pub mod garble;
